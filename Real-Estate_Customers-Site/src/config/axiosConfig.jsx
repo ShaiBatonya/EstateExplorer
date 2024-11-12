aace01c59aaf41ee893aa2ba7a7d945289efce9c
Shay_Batonya <127010513+ShaiBatonya@users.noreply.github.com>
2024-11-12 15:39:05 +0200

    Added proxy for the server URL

--- Real-Estate_Customers-Site/src/config/axiosConfig.jsx
+++ Real-Estate_Customers-Site/src/config/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL,
+    baseURL: "https://real-estate-mern-stack-production.up.railway.app",
     headers: {
         "Content-Type" : "application/json",
     },
